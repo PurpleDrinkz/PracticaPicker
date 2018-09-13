a16a54363423404e15897d94a409529525eca31a
Alumno <ulsa@Obed-Miranda.local>
2018-09-13 13:39:09 -0700

    Picker de animales salvajes

--- PracticaPicker/ViewController.swift
+++ PracticaPicker/ViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    let colores = ["Orca", "Rino", "Lobo", "Leon", "Búho", "Delfín", "Elefantes", "Gorila", "Jirafa"]
+    
+    @IBOutlet weak var imgFoto: UIImageView!
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colores.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colores[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        switch row
+        {
+        case 0: imgFoto.image = UIImage(named: "orca")
+        case 1: imgFoto.image = UIImage(named: "rino")
+        case 2: imgFoto.image = UIImage(named: "lobo")
+        case 3: imgFoto.image = UIImage(named: "leon")
+        case 4: imgFoto.image = UIImage(named: "buho")
+        case 5: imgFoto.image = UIImage(named: "delfin")
+        case 6: imgFoto.image = UIImage(named: "elefante")
+        case 7: imgFoto.image = UIImage(named: "gorila")
+        case 8: imgFoto.image = UIImage(named: "jirafa")
+       
+            
+        default:
+            break
+        }
+        
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imgFoto.image = UIImage(named: "orca")
     }
 
     override func didReceiveMemoryWarning() {
